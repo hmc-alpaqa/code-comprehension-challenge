1c5c7f8f59e74f85686babc054eb56b1478a9373
Diego Esparza <dae1@williams.edu>
2021-08-02 15:22:31 -0400

    changing local db type

--- evaluate_tests/settings.py
+++ evaluate_tests/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'evaluate_tests.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_db',
+        'USER': 'hero',
+        'PASSWORD': 'my_db@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
