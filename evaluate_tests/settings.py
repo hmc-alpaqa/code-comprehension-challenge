9aaf514fbfd37cbb25e905758d89e9136006b239
Diego Esparza <dae1@williams.edu>
2021-08-02 15:26:17 -0400

    remove port config

--- evaluate_tests/settings.py
+++ evaluate_tests/settings.py
@@ -88,10 +88,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'my_db',
-        'USER': 'hero',
-        'PASSWORD': 'my_db@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
     }
 }
 
