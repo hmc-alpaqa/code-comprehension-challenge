edcfe03f926cea2ca2241ecc138bae19eaeb04df
Diego Esparza <dae1@williams.edu>
2021-08-03 10:17:31 -0400

    remove uuid assignment

--- challenge/views.py
+++ challenge/views.py
@@ -8,7 +8,7 @@ import random
 import re
 from random_username.generate import generate_username
 
-import uuid
+
 
 def login_or_new(request):
     return render(request, 'login_or_new.html')
@@ -145,7 +145,7 @@ def create_key(request, origin):
 
     if attempted_user:
         if attempted_user not in taken:
-            created_user = User(char_name=attempted_user, id=uuid.uuid4())
+            created_user = User(char_name=attempted_user)
             created_user.save()
             return select_challenge_page(request, "no type",
                                          "create_key.html",
